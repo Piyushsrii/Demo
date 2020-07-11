96610c3aee3fe436f2f70d9f41449b6632880c23
piyushsrii <piyushnamo888@gmail.com>
2020-07-12 00:46:17 +0530

    commit in uc2

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 echo Welcome in master
-
-
-
-#######################usercase2################33
-
-hey i am also here
+#!/bin/bash -x
+ isPartTime=1;
+ isFullTime=2;
+ empRatePerHr=20;
+ randomCheck=$((RANDOM%3));
+     if [ $isFullTime -eq $randomCheck ];
+then
+        empHrs=8;
+elif     [ $isFullTime -eq $randomCheck ];
+then
+           empHrs=4;
+else
+         empHrs=0;
+fi
+    salary=$(($empHrs*$empRatePerHr));
+      echo $salary
 
 
